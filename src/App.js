8b1fadd8089f663fa692f1d139651b007221c019
collerhatguy <jacobshomali3@gmail.com>
2020-08-14 06:25:39 -0400

    sv

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./App.css";
+import Button from "./Button";
+
+function App() {
+  const [display, setDisplay] = useState("");
+  function update() {
+    setDisplay((prevDisplay) => (prevDisplay += "1"));
+  }
+  return (
+    <>
+      <div>
+        <h1>{display}</h1>
+        <Button onclick={update} />
+      </div>
+    </>
+  );
+}
+
+export default App;
