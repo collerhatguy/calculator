279e937b3ca16002df6aa9315e4e2169f369bb9a
collerhatguy <jacobshomali3@gmail.com>
2020-08-17 13:25:49 -0400

    syntax

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setDisplay(display + "*")}>*</button>
         <button onClick={() => setDisplay(display + ".")}>.</button>
         <button onClick={() => setDisplay(display + "0")}>0</button>
-        <button onClick={() => setDisplay((display = ""))}>ac</button>
+        <button onClick={() => setDisplay("")}>ac</button>
         <button onClick={() => setDisplay(display + "=")}>=</button>
       </div>
     </>
