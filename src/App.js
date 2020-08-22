86e5379dbf5083ab35039f553b61b09f191aa5c9
collerhatguy <jacobshomali3@gmail.com>
2020-08-22 08:52:22 -0400

    delete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import { evaluate } from "mathjs";
 
 function App() {
   const [display, setDisplay] = useState("");
-  const [answer, setAnswer] = useEffect(
-    function () {
-      setAnswer(evaluate(display));
-    },
-    [display]
-  );
+  const [answer, setAnswer] = useState(0);
+  useEffect(setAnswer(evaluate(display)), [display]);
   return (
     <>
       <div id="displayContainer">
@@ -19,20 +14,45 @@ function App() {
         </h1>
       </div>
       <div id="calculatorContainer">
-        <button onClick={() => setDisplay(display + "1")}>1</button>
-        <button onClick={() => setDisplay(display + "2")}>2</button>
-        <button onClick={() => setDisplay(display + "3")}>3</button>
-        <button onClick={() => setDisplay(display + "+")}>+</button>
-        <button onClick={() => setDisplay(display + "4")}>4</button>
-        <button onClick={() => setDisplay(display + "5")}>5</button>
-        <button onClick={() => setDisplay(display + "6")}>6</button>
-        <button onClick={() => setDisplay(display + "-")}>-</button>
-        <button onClick={() => setDisplay(display + "7")}>7</button>
-        <button onClick={() => setDisplay(display + "8")}>8</button>
-        <button onClick={() => setDisplay(display + "9")}>9</button>
-        <button onClick={() => setDisplay(display + "*")}>*</button>
-        <button onClick={() => setDisplay(display + ".")}>.</button>
-        <button onClick={() => setDisplay(display + "0")}>0</button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "1")}>
+          1
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "2")}>
+          2
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "3")}>
+          3
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "4")}>
+          4
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "5")}>
+          5
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "6")}>
+          6
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "7")}>
+          7
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "8")}>
+          8
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "9")}>
+          9
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "0")}>
+          0
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + ".")}>
+          .
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "/")}>
+          /
+        </button>
+        <button onClick={() => setDisplay((prevDisplay) => prevDisplay + "*")}>
+          *
+        </button>
         <button onClick={() => setDisplay("")}>ac</button>
         <button onClick={() => setDisplay(evaluate(display))}>=</button>
       </div>
