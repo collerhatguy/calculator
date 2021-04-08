28afee3136a2f18d408a2f196e69c4fd003cfb3c
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-08 12:13:45 -0400

    about to test component now

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function ButtonList({inputs}) {
+export default function ButtonList({inputs, setDisplay}) {
     return (
         <div>
-            
+            {inputs.map((input, index) => {
+                return <Input 
+                            input={input} 
+                            setDisplay={() => setDisplay(prevDisplay => prevDisplay + input)} 
+                            key={index}
+                        />
+            })}
         </div>
     )
 }
---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Input({input, setDisplay}) {
+    return (
+        <button onClick={() => setDisplay()}>
+            {input}
+        </button>
+    )
+}
