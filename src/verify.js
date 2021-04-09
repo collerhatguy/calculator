b365ec97f51c96796e67d1003a604bd27244ad68
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-09 11:01:28 -0400

    bulding blocks have been set

--- src/verify.js
+++ src/verify.js
@@ -2,29 +2,36 @@
 // 1. a varible that says whether its valid
 // 2. an error message
 // 3. a function that rechecks whether the diplay is valid
+import {useState} from "react"
 
 const invalidEndInputs = [ "*", "^", ".", "-", "/", "(",]
 const invalidStartInputs = [ "*", "^", ".", "-", "/", ")"]
-const [error, setError] = useState("");
+
+const [errorMessage, setErrorMessage] = useState("");
+const [validity, setValidity] = useState(true);
 
 // for stopping invalid inputs
 const verifyInput = (answer) => {
     const firstCharacter = answer.substring(0,1);
     const lastCharacter = answer.substring(answer.length - 1);
     if (answer.length === 0) {
-        setError("You cannot send nothing");
-        return false;
+        setErrorMessage("You cannot send nothing");
+        setValidity(false);
     }
     // if ending in any invalid expression end accordingly
     if (invalidEndInputs.includes(lastCharacter)) {
-        setError("You cannot end a subbmission that way");
-        return false;
+        setErrorMessage("You cannot end a subbmission that way");
+        setValidity(false);
     }
     // if starts off invalid then return invalid
     if (invalidStartInputs.includes(firstCharacter)) {
-        setError("You cannot start a subbmission that way");
-        return false;
+        setErrorMessage("You cannot start a subbmission that way");
+        setValidity(false);
+    } else {
+        setErrorMessage("");
+        setValidity(true);
     }
-    setError("")
-    return true;;
+}
+const useVerify = () => {
+    
 }
