800d9eeae0b1010b869bdc8ca4a3c76de6b364d3
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-09 09:59:19 -0400

    error variable doesn render for some reason. going to modulate for simplicity

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "./App.css"
 function App() {
   const [display, setDisplay] = useState("");
   const [answer, setAnswer] = useState("");
+  const [error, setError] = useState("");
   const findAnswer = () => {
+    if (!verifyInput(answer)) return;
     setAnswer(evaluate(display))
     setDisplay(evaluate(display))
   }
@@ -13,11 +15,31 @@ function App() {
     setAnswer("")
     setDisplay("")
   }
+  // fro stopping invalid inputs
+  const verifyInput = (answer) => {
+    // if ending in any invalid expression end accordingly
+    if (invalidEndInputs.includes(answer.substring(answer.length - 2, answer.length - 1))) {
+      setError("You cannot end a subbmission that way");
+      return false;
+    }
+    // if starts off invalid then return invalid
+    if (invalidStartInputs.includes(answer.substring(0, 1))) {
+      setError("You cannot start a subbmission that way");
+      return false;
+    }
+    setError("")
+    return true;
+  }
   const inputs = [ "*", "^", ".", "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "/", "(", "0", ")",]
+  const invalidEndInputs = [ "*", "^", ".", "-", "/", "(",]
+  const invalidStartInputs = [ "*", "^", ".", "-", "/", ")"]
   return (
     <>
       <div id="displayContainer">
         <h2>
+          <div>
+            {error}
+          </div>
           <div>
             {display} =
           </div>
@@ -29,6 +51,7 @@ function App() {
       <div id="calculatorContainer">
         <ButtonList inputs={inputs} setDisplay={setDisplay} allClear={allClear} findAnswer={findAnswer}/>
       </div>
+
     </>
   );
 }
